077fee4e341655162bcd4c529e67a3a4687202ac
ashuzamaheshe4 <ashuzamaheshe4@gmail.com>
2024-04-14 12:06:48 +0200

    Migrations

---
+++ database/migrations/2024_04_14_094138_create_categorypost_post_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Category;
+use App\Models\Post;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categorypost_post', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Post::class)->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignIdFor(Category::class)->cascadeOnDelete()->cascadeOnUpdate();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categorypost_post');
+    }
+};
